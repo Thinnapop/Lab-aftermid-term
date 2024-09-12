d0001243f8ce56e28a5016c04a717e050767408f
DiceX <149762497+Thinnapop@users.noreply.github.com>
2024-09-12 15:39:44 +0700

    Update convertTem.java

--- method/convertTem.java
+++ method/convertTem.java
@@ -4,36 +4,48 @@ import java.util.Scanner;
 
 public class convertTem {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the current scale c(Celsius) / f(fahrenheit) : ");
-        char currentScale = Character.toUpperCase(scanner.next().charAt(0));
+        Scanner input = new Scanner(System.in);
+        String userResponse;
+        do {
+            System.out.println("Enter the current scale C(Celsius) F(fahrenheit) : ");
+            char currentScale = Character.toUpperCase(input.next().charAt(0));
 
-        System.out.println("Enter the target Scale c(Celsius) / f(fahrenheit) : ");
-        char targetScale = Character.toUpperCase(scanner.next().charAt(0));
+            System.out.println("Enter the target scale C(Celsius) F(fahrenheit) : ");
+            char targetScale = Character.toUpperCase(input.next().charAt(0));
 
-        System.out.println("Enter the temperature : ");
-        double temperature = scanner.nextDouble();
+            System.out.println("Enter the temperature : ");
+            double temperature = input.nextDouble();
 
-        String result = formula(currentScale, targetScale, temperature);
-        System.out.println(result);
+            if (currentScale == targetScale || targetScale == currentScale) {
+                System.out.println("Scale can't be the same!! ");
+            }
+            else{
+            String result = convertedTemp(currentScale, targetScale, temperature);
+            System.out.println(result);
+        }
+
+        System.out.println("Do you want to perform another conversion? (yes/no): ");
+        userResponse = input.next().trim().toLowerCase();
+
+    }
+    while (userResponse.equals("yes"));
+        System.out.println("Program terminated");
+        
 
     }
 
-    public static String formula(char a, char b, double temperature) {
+    public static String convertedTemp(char currentScale, char targetScale, double temperature) {
         double converted;
-
-        if (a == 'C') {
-            converted = (temperature - 32) * 5 / 9;
-            return temperature + " C is " + converted + " F";
-        }
-         else if (a == 'F') {
+        if (currentScale == 'C' && targetScale == 'F') {
             converted = ((9 * temperature) / 5) + 32;
-            return temperature + " F is " + converted + " C";
+            return "the convertion of " + temperature + " is : " + converted;
         }
-        
+         else if (currentScale == 'F' && targetScale == 'C') {
+            converted = ((temperature - 32) * 5 / 9);
+            return "the convertion of " + temperature + " is : " + converted;
+        }
          else {
-            return "invalid scale please enter C or F";
+            return "unexpect error";
         }
-
     }
 }
